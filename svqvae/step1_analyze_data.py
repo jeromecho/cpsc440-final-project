ea8288b0d4f925a9a2fa3f13a2d4db5087d9b036
Jerome Cho <jeromejjcho@gmail.com>
2025-04-23 14:56:38 -0700

    get pretraining code alright

--- svqvae/step1_analyze_data.py
+++ svqvae/step1_analyze_data.py
@@ -3,8 +3,6 @@ from PIL import Image
 import os
 import random
 
-# For CAM16 TIFFs
-Image.MAX_IMAGE_PIXELS = None
 
 class PretrainingDataset(Dataset):
     def __init__(self, img_dir, transform=None, crop_size=None):
@@ -16,7 +14,7 @@ class PretrainingDataset(Dataset):
             os.path.join(img_dir, fname)
             for fname in os.listdir(img_dir)
             if fname.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff'))
-        ][:1]
+        ]
 
     def __len__(self):
         return len(self.image_paths)
@@ -26,9 +24,6 @@ class PretrainingDataset(Dataset):
         print(f"img_path {img_path}")
 
         with Image.open(img_path) as img:
-            width, height = img.size
-            image = None
-
 
             image = img.convert('RGB')
 
